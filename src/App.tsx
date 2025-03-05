790b8ec71389d8f277601129c0a1bef5331800ca
Sahil <bagadesahil31@gmail.com>
2025-03-05 13:59:34 +0530

    new respo commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home.tsx';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage.tsx';
 
 function App() {
   return (
-    <div className="App">
-      <h2 className="App-h2">Jobs  Messages   Perfomances Cards</h2>
-      <Home />
-    </div>
+    // <h2 className="App-h2">Jobs  Messages   Perfomances Cards</h2>
+<BrowserRouter>
+   <Routes>   
+    <Route path='/' element={ <Home/>}/>
+    <Route path='/contact' element={ <ContactPage/>}/>
+   </Routes>
+</BrowserRouter>
+  
   );
 }
 
--- src/services/cardApi.ts
+++ src/services/cardApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getCard = async() => {
   try{
-    const response = await axios.get("http://localhost:5001/rankings"); 
+    const response = await axios.get("http://localhost:5010/rankings"); 
     return response.data;
   } catch (error) {
     console.log(error);
